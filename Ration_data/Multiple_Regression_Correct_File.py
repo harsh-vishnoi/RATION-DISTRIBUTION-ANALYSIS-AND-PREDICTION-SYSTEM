b28cd9057880229df5511f15fdac9480f0b9e1db
kanishkagarg <41885061+kanishkagarg@users.noreply.github.com>
2018-10-12 13:24:00 +0530

    Create Multiple_Regression_Correct_File.py
    Master branch can only delete files. Delete the old one

---
+++ Ration_data/Multiple_Regression_Correct_File.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('/Users/kanishka/Desktop/Codes/Minor Project Final/RATION-DISTRIBUTION-ANALYSIS-AND-PREDICTION-SYSTEM-master-2/Ration_data/Test_Data.csv')
+
+X = dataset.iloc[:,4:6].values
+y = dataset.iloc[:,8].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 1] = labelencoder_X.fit_transform(X[:, 1])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding Dummy Variable Trap
+
+X = X[:,2:]
+
+#Splitting Data to Training and Test Set
+
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=0)
+
+#Fitting Multiple Linear Regression To Training Set
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting results on Test set
+y_pred = regressor.predict(X_test)
+
+
