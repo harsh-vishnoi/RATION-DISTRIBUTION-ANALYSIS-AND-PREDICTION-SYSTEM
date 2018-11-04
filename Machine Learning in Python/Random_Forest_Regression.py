82d1c3ba2683a388b882c85231419ed5ec689993
Kanishka Garg <kanishka080199@gmail.com>
2018-11-04 14:51:19 +0530

    Completed the Regression Models

---
+++ Machine Learning in Python/Random_Forest_Regression.py
@@ -0,0 +1,69 @@
+#Random Forest Regression
+
+#Regression Template
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the Dataset
+dataset = pd.read_csv('Test_data.csv')
+X = dataset.iloc[:,4:6].values
+y = dataset.iloc[:,8:9].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 1] = labelencoder_X.fit_transform(X[:, 1])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding Dummy Variable Trap
+
+X = X[:,2:]
+
+
+#Splittung the data into training and test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test= train_test_split(X,y,test_size=0.3,random_state=0)
+
+
+#Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+#Fitting Random Forest Regressor Model to Dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 500, random_state=0)
+regressor.fit(X_train,y_train)
+
+#Predicting a new result 
+y_pred = regressor.predict([0,18])
+
+
+#Visualising the Random Forest Regression Results
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x = X_train[:,0]
+y = X_train[:,1]
+z = y_train
+
+ax.scatter(x, y, z, c='r', marker='o')
+
+ax.set_title(' Regression Model  ')
+ax.set_xlabel(' Male/Female')
+ax.set_ylabel(' Age ')
+ax.set_zlabel(' Calories ')
+
+x1 = X_test[:,0]
+y1 = X_test[:,1]
+z1 = regressor.predict(X_test)
+ax.scatter(x1, y1, z1, c='b', marker='o')
+plt.show()
+
