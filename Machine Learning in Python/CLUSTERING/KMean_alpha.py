3ad38f375b7a5350eabfc6f8eb20ffd8cf599f2b
Kanishka Garg <kanishka080199@gmail.com>
2018-11-17 00:18:53 +0530

    Cluster Column added in Answer.csv file in ML IN PYTHON/CLUSTERING folder

--- Machine Learning in Python/CLUSTERING/KMean_alpha.py
+++ Machine Learning in Python/CLUSTERING/KMean_alpha.py
@@ -6,8 +6,8 @@ import pandas as pd
 
  #Importing Dataset
  
-dataset = pd.read_csv('Desktop/PROJECTS/RATION-DISTRIBUTION-ANALYSIS-AND-PREDICTION-SYSTEM/Machine Learning in Python/CLUSTERING/compressed_data.csv')
-X = dataset.iloc[:,[4,9,11,12,13]].values
+dataset = pd.read_csv('Answer.csv')
+X = dataset.iloc[:,[11,19]].values
 
 #Using Elbow method to find the number of clusters
 from sklearn.cluster import KMeans
@@ -24,7 +24,22 @@ plt.ylabel('WCSS')
 plt.show()
 
 #Applying K-Means to the dataset
-kmeans = KMeans(n_clusters = 5, init = 'k-means++',max_iter = 300, n_init = 10,random_state = 0)
+kmeans = KMeans(n_clusters = 8, init = 'k-means++',max_iter = 300, n_init = 10,random_state = 0)
 y_kmeans =  kmeans.fit_predict(X)
 
+#Visualising the Clusters
+plt.scatter(X[y_kmeans == 0 , 0],X[y_kmeans == 0 , 1], s = 20, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1 , 0],X[y_kmeans == 1 , 1], s = 20, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2 , 0],X[y_kmeans == 2 , 1], s = 20, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3 , 0],X[y_kmeans == 3 , 1], s = 20, c = 'brown', label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4 , 0],X[y_kmeans == 4 , 1], s = 20, c = 'black', label = 'Cluster 5')
+plt.scatter(X[y_kmeans == 5 , 0],X[y_kmeans == 5 , 1], s = 20, c = 'orange', label = 'Cluster 6')
+plt.scatter(X[y_kmeans == 6 , 0],X[y_kmeans == 6 , 1], s = 20, c = 'pink', label = 'Cluster 7')
+plt.scatter(X[y_kmeans == 7 , 0],X[y_kmeans == 7 , 1], s = 20, c = 'purple', label = 'Cluster 8')
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1],s = 100, c = 'yellow', label = 'Centroids')
+plt.title('Clusters')
+plt.xlabel('Salary')
+plt.ylabel('Calories')
+plt.legend()
+plt.show() 
 
