dbe340434fe2630ddc81c797f042bfbee3dfd045
Kanishka Garg <kanishka080199@gmail.com>
2018-11-16 19:20:38 +0530

    Added Clustering Python

--- Machine Learning in Python/REGRESSION_SECOND/SVR.py
+++ Machine Learning in Python/REGRESSION_SECOND/SVR.py
@@ -15,13 +15,7 @@ from sklearn.cross_validation import train_test_split
 X_train,X_test,y_train,y_test= train_test_split(X,y,test_size=0.3,random_state=0)
 
 
-#Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)
+
 
 #Fitting SVR to Dataset
 from sklearn.svm import SVR
@@ -56,3 +50,4 @@ with open('Answer.csv','w') as csvfile:
     csvwriter.writerow(fields)
     csvwriter.writerows(rows)
     
+
